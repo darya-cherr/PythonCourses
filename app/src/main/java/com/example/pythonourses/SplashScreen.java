2b3e19dabb06b7bda09eff94b68a62b027a73f0d
darya-cherr <chernikovadarya67@gmail.com>
2022-04-12 01:59:32 +0300

    Added splash screen and google auth with Firebase

--- app/src/main/java/com/example/pythonourses/SplashScreen.java
+++ app/src/main/java/com/example/pythonourses/SplashScreen.java
@@ -1,14 +1,148 @@
 package com.example.pythonourses;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
+    private Animation zoom;
+    private Animation signInBtnMove;
+
+    private ImageView logo;
+    private AppCompatButton signInButton;
+    private GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth mAuth;
+
+    private boolean hasEnded;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        zoom = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.zoom);
+        signInBtnMove = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.sign_in_btn_anim);
+
+        logo = findViewById(R.id.logo);
+        signInButton = findViewById(R.id.signInButton);
+
+        if(user != null) signInButton.setVisibility(View.GONE);
+
+        logo.startAnimation(zoom);
+        signInButton.startAnimation(signInBtnMove);
+
+        zoom.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                if(user != null){
+                    signInButton.setVisibility(View.GONE);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signIn();
+            }
+        });
+
+        createRequest();
+    }
+
+
+
+    private void createRequest() {
+        GoogleSignInOptions gso = new GoogleSignInOptions
+                .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("354103113960-q0tl5702kup89cksj9k2bc0mot7t7fgr.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    private void signIn(){
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuth(account);
+            } catch (ApiException e) {
+                Toast.makeText(this, e.getMessage(),Toast.LENGTH_LONG).show();
+                Log.d("MyLog", e.getMessage());
+            }
+        }
+    }
+
+    private void firebaseAuth(GoogleSignInAccount account) {
+        AuthCredential firebaseCredential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        mAuth.signInWithCredential(firebaseCredential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(SplashScreen.this,"Ошибка" ,Toast.LENGTH_LONG).show();
+                            Log.d("MyLog", "Error");
+                        }
+                    }
+                });
     }
 }
