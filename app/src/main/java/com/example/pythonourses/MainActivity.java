29e3d94d1a9b77215f39390da44c3dbe9cc1a804
darya-cherr <chernikovadarya67@gmail.com>
2022-04-13 12:35:51 +0300

    Added navigation bar

--- app/src/main/java/com/example/pythonourses/MainActivity.java
+++ app/src/main/java/com/example/pythonourses/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.pythonourses;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ChipNavigationBar navigationBar;
+    private FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        navigationBar = findViewById(R.id.navigation_bar);
+
+        if(savedInstanceState == null){
+            navigationBar.setItemSelected(R.id.home, true);
+            fragmentManager = getSupportFragmentManager();
+            HomeFragment homeFragment = new HomeFragment();
+            fragmentManager.beginTransaction().replace(R.id.fragment_container, homeFragment).commit();
+        }
+
+        navigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int id) {
+                Fragment fragment = null;
+                switch(id){
+                    case R.id.home:
+                        fragment = new HomeFragment();
+                        break;
+                    case R.id.tasks:
+                        fragment = new TasksFragment();
+                        break;
+                    case R.id.lectures:
+                        fragment = new LecturesFragment();
+                        break;
+                }
+
+                if(fragment != null){
+                    fragmentManager = getSupportFragmentManager();
+                    fragmentManager.beginTransaction().replace(R.id.fragment_container, fragment).commit();
+                }
+            }
+        });
+
     }
 }
